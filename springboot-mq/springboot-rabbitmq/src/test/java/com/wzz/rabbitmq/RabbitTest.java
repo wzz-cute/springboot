10b1581be717a43fbdcab7b4298aec763db00b42
wzz <3325667808@qq.com>
2022-01-29 21:21:38 +0800

    rabbitmq基本完成2

---
+++ springboot-mq/springboot-rabbitmq/src/test/java/com/wzz/rabbitmq/RabbitTest.java
@@ -0,0 +1,51 @@
+package com.wzz.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitTest {
+    //主要发送消息
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    //主要创建交换机 ，队列 ，绑定
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    //创建队列
+    @Test
+    public void createQueue() {
+        Queue queue = new Queue("test-jiajia", true, false, false);
+        String s = amqpAdmin.declareQueue(queue);
+        System.out.println(s);
+    }
+
+    //创建交换机
+    @Test
+    public void createExchanges() {
+        Exchange directExchange = new DirectExchange("ex-jiajia", true, false);
+        amqpAdmin.declareExchange(directExchange);
+    }
+
+    //创建绑定关系
+    @Test
+    public void createBinding() {
+        Binding binding = new Binding("test-jiajia", Binding.DestinationType.QUEUE,
+                "ex-jiajia", "hello.jiajia", null);
+        amqpAdmin.declareBinding(binding);
+    }
+
+    @Test
+    public void sendMsg(){
+        rabbitTemplate.convertAndSend("ex-jiajia","hello.jiajia", "hello3 ====>> abcdefg");
+    }
+
+
+}
