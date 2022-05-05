1fdf242529084a4a9db85b22ebd5846095805fcc
wzz <3325667808@qq.com>
2022-05-06 07:40:34 +0800

    add to one

--- springboot-io/src/test/java/com/wzz/io/SpringbootIoApplicationTests.java
+++ springboot-io/src/test/java/com/wzz/io/SpringbootIoApplicationTests.java
@@ -3,11 +3,15 @@ package com.wzz.io;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootTest
 class SpringbootIoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        boolean newFile = new File("a.html").createNewFile();
     }
 
 }
